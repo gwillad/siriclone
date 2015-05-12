f33b082fe639a41ce5daffddbdbb7a1ab7526ee1
Casey Collins <chcollin@gemini.cs.hamilton.edu>
2015-05-12 14:49:55 -0400

    add wolfram api file

---
+++ wolfram_questions.py
@@ -0,0 +1,5 @@
+###################################################################
+# Casey Collins, Adam Gwilliam, Zach Arnold
+# wolfram_alpha_api.py
+# This will call and search the wolfram alpha api
+###################################################################
