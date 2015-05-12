959e408dfd16cfe2dd4b2eb27a92ebfafab389d6
Casey Collins <chcollin@gemini.cs.hamilton.edu>
2015-05-12 19:46:56 -0400

    Add basic wolfram functionality

--- wolfram_questions.py
+++
@@ -1,5 +0,0 @@
-###################################################################
-# Casey Collins, Adam Gwilliam, Zach Arnold
-# wolfram_alpha_api.py
-# This will call and search the wolfram alpha api
-###################################################################
