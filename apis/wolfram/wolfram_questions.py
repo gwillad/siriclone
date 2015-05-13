69507e87b5b4703c242ea523e1e2eded58a9b83a
Casey Collins <chcollin@gemini.cs.hamilton.edu>
2015-05-13 00:24:09 -0400

    Add google searching and other minor changes

--- apis/wolfram/wolfram_questions.py
+++ apis/wolfram/wolfram_questions.py
@@ -9,6 +9,12 @@
 
 from __future__ import unicode_literals
 
+import urllib
+import simplejson
+import sys
+from subprocess import call
+
+
 import pytest
 import random
 import wolframalpha
@@ -19,11 +25,15 @@ from sys import argv
 app_id = 'Q59EW4-7K8AHE858R'
 "App ID for testing this project. Please don't use for other apps."
 
-answer_leads = ["The answer you are looking for is ", "I think you wanted ", "Hi, I'm pretty sure you were looking for "]
+answer_leads = ["The answer you are looking for is ", "I think you wanted to find ", "Hi, I'm pretty sure you were looking for ", "Your answer is "]
 
 arithmetic_leads = ["The answer to problem you asked is ", "The solution to your arithmetic query is "]
 
 client = wolframalpha.Client(app_id)
+
+def google_api(query):
+    call(["python2.7", "../search.py", "google", query])
+
 
 def arithmetic_query(query):
     res = client.query(query_wolf)
@@ -32,6 +42,7 @@ def arithmetic_query(query):
         print random.choice(arithmetic_leads) + results[0].text
     else:
         print "I can't find anything on what you are looking for right now. Let me try and find some web pages that may help you"
+        google_api(query)
 
 def normal_query(query):
     res = client.query(query_wolf)
@@ -40,6 +51,7 @@ def normal_query(query):
         print random.choice(answer_leads) + results[0].text
     else:
         print "I can't find anything on what you are looking for right now. Let me try and find some web pages that may help you"
+        google_api(query)
     
 query_wolf = " ".join(argv[1:])
 
